bce7ce5e0be967e2f19f38448fbc7766e6f570e3
Fabboy <Fabboynice@gmail.com>
2022-10-15 12:10:58 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+
+
+DWORD GetProcId(const char *procName) {
+    DWORD procId = 0;
+    HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hSnap != INVALID_HANDLE_VALUE) {
+        PROCESSENTRY32 procEntry;
+        procEntry.dwSize = sizeof(procEntry);
+        if (Process32First(hSnap, &procEntry)) {
+            do {
+                if (!_stricmp(procEntry.szExeFile, procName)) {
+                    procId = procEntry.th32ProcessID;
+                    break;
+                }
+            } while (Process32Next(hSnap, &procEntry));
+        }
+    }
+    CloseHandle(hSnap);
+    return procId;
+}
+
+
+int main() {
+    const char *dllPath = "C:\\Users\\Fabri\\source\\repos\\ACHack\\Debug\\ACHack.dll";
+    const char* procName = "ac_client.exe";
+    DWORD procId = 0;
+
+    while (!procId) {
+        procId = GetProcId(procName);
+        Sleep(30);
+    }
+    HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, 0, procId);
+    if (hProc && hProc != INVALID_HANDLE_VALUE) {
+        void *loc = VirtualAllocEx(hProc, 0, MAX_PATH, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+        WriteProcessMemory(hProc, loc, dllPath, strlen(dllPath) + 1, 0);
+        HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE) LoadLibraryA, loc, 0, 0);
+        if (hThread) {
+            CloseHandle(hThread);
+        }
+    }else{
+        std::cout << "Error: " << GetLastError() << std::endl;
+    }
+
+    //check if dll is injected
+    if (hProc && hProc != INVALID_HANDLE_VALUE) {
+        std::cout << "Dll injected" << std::endl;
+    }
+    else {
+        std::cout << "Dll not injected" << std::endl;
+    }
+
+
+    CloseHandle(hProc);
+    return 0;
+}
